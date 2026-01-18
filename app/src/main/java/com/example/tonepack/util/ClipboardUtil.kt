aea1e18fc7abfc7ea0d17fc006c06a8b0a90cc81
seoyeon <njtbear3@gmail.com>
2026-01-19 01:17:47 +0900

    복사하기 기능을 위해 미리 만든 공통 함수 미리 만들기

--- app/src/main/java/com/example/tonepack/util/ClipboardUtil.kt
+++ app/src/main/java/com/example/tonepack/util/ClipboardUtil.kt
@@ -1,4 +1,9 @@
 package com.example.tonepack.util
 
-class ClipboardUtil {
+object ClipboardUtil {
+    fun copyToClipboard(context: android.content.Context, text: String) {
+        val clipboard = context.getSystemService(android.content.Context.CLIPBOARD_SERVICE) as android.content.ClipboardManager
+        val clip = android.content.ClipData.newPlainText("TonePack Copy", text)
+        clipboard.setPrimaryClip(clip)
+    }
 }
