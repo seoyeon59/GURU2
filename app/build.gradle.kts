0007401001b9e2b345a8f4b90d52a5738eb5858b
seoyeon <njtbear3@gmail.com>
2026-01-18 22:32:08 +0900

    build.gradle.kts 기존 라이브러리 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,4 +57,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.android.material:material:1.12.0")
 }
